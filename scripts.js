8913a00e2d86beb35b3413274b66cbabd8363f85
curly08 <mattynoakes08@gmail.com>
2022-01-23 14:26:02 -0500

    Allow users to add new book upon form submit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-let myLibrary = [];
-
+// Book Object
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -19,25 +18,28 @@ Book.prototype.getInfo = function() {
   };
 };
 
-function displayBooks() {
+function displayBooks(...args) {
   const library = document.querySelector('.library');
 
-  myLibrary.forEach((book) => {
-    const article = document.createElement('article');
-    library.appendChild(article);
+  args.forEach((book) => {
     const title = document.createElement('h2');
     title.textContent = book.title;
+    const article = document.createElement('article');
+    library.appendChild(article);
     const list = document.createElement('ul');
     const liAuthor = document.createElement('li');
     liAuthor.textContent = `Author: ${book.author}`;
     const liPages = document.createElement('li');
     liPages.textContent = `Number of Pages: ${book.pages}`;
     const liRead = document.createElement('li');
-    liRead.textContent = `Status: ${book.read ? 'Read' : 'Not Read'}`;
+    liRead.textContent = `Status: ${book.read ? 'Read' : 'Not Read Yet'}`;
     article.append(title, list);
     list.append(liAuthor, liPages, liRead);
   });
 };
+
+// mock data
+let myLibrary = [];
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 295, 1);
 const theFellowship = new Book('The Fellowship of the Ring', 'J. R. R. Tolkien', 423, 1);
@@ -45,15 +47,28 @@ const theTowers = new Book('The Two Towers', 'J. R. R. Tolkien', 352, 0);
 const theReturn = new Book('The Return of the King', 'J. R. R. Tolkien', 416, 0);
 
 addToLibrary(theHobbit, theFellowship, theTowers, theReturn);
-displayBooks();
+
+myLibrary.forEach((book) => {
+  displayBooks(book);
+})
 
 // Add Book Form
-function showForm() {
-  const newBookForm = document.getElementById('new-book-form');
+const newBookForm = document.getElementById('new-book-form');
 
+function showForm() {
   if (newBookForm.style.display === "block") {
     newBookForm.style.display = "none";
   } else {
     newBookForm.style.display = "block";
   };
 };
+
+newBookForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  addToLibrary(new Book(newBookForm.elements['title'].value,
+               newBookForm.elements['author'].value,
+               newBookForm.elements['pages'].value,
+               newBookForm.elements['read-status'].checked));
+  displayBooks(myLibrary[myLibrary.length - 1]);
+  this.reset();
+});
